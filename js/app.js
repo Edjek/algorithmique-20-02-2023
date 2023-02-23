76ae7e02964154a527aadcddc8e120f9b8688c9f
Edjek <edjek@hotmail.fr>
2023-02-23 17:31:21 +0100

    algorithmique

--- js/app.js
+++ js/app.js
@@ -1,224 +1,166 @@
-/* ------------------ Les commentaires ------------------ */
+/* --------------------------- Les commentaires --------------------------- */
 
-// Un commentaire en Javascript
-/*
+// Un commentaire sur une ligne en Javascript
+/* 
     Un commentaire multiligne
-    en javascript
-*/
+    en Javascript 
+ */
 
-/* ------------------ Les variables ------------------ */
+/* --------------------------- Affichage --------------------------- */
 
-// Déclaration d'une variable
+//Afficher des informations dans la console de debbugage du navigateur
+console.log('Hello World!');
+
+//Afficher une boite de dialogue avec champs à remplir
+prompt('Quel age avez-vous ?');
+
+/* --------------------------- Les Variables --------------------------- */
+
+//  Déclaration d'une Variable
 let uneVariable;
 
-// Affectation de la variable
-uneVariable = 2;
+// Affectation d'une variable
+uneVariable = 'Je suis une variable!';
 
-// Réaffectation d'une variable (on change sa valeur)
-uneVariable = 8;
+// Les 2 se font généralement en même temps
+// Les variables sont sensibles à la casse (majuscules/minuscules)
+let helloWorld = 'Hello world!';
 
-// Les 2 peuvent se faire en même temps
-// Les variables sont sensibles à la casse (majuscule/minuscule)
-let helloWorld = 'Hello, World';
-
-// Affichage d'un message dans la console
-// console.log('Je suis un message affiché dans la console');
 // Affichage d'une variable dans la console
-// console.log(helloWorld);
+console.log(uneVariable);
+console.log(helloWorld);
 
 // Affectation d'une variable dans une autre variable
-let test = helloWorld;
-// console.log(test)
+let affectation = helloWorld;
+console.log(affectation);
 
-// Il existe plusieurs types de variables (entiers, réels, chaînes de caractère, booléens, etc...)
-let number = 23;
-let decimal = 18.5;
-let varchar = 'je suis un message';
-// true ou false
+/* --------------------------- La Concatenation --------------------------- */
+
+let nom = 'EDJEKOUANE';
+let prenom = 'Rachid';
+
+// Concatenation (Afficher du texte et des variables)
+console.log("Je m'appelle " + nom + ' ' + prenom);
+
+// Déclarer une chaine de caractère, un nombre, booléen
+let varchar = 'salut tout le monde';
+let number = 2;
+let decimal = 20.3;
 let boolean = true;
 
-// Comment utiliser les guillemets dans une chaine de caractère(les doubles guillemets)
-let message = "J'apprend l'algorithmique";
+// Les opérations mathématiques (+, -, *, / )
+let resultat = number * decimal;
+console.log(resultat);
 
-/* ------------------ La Concaténation ------------------ */
+// Comment utiliser les guillemets dans une chaine de caractère
+let message = 'J\'apprend l\'algorithmie';
+console.log(message);
 
-let firstName = 'Rachid';
-let lastName = 'EDJEKOUANE';
-let age = 39;
-// console.log("Bonjour, je m'appelle " + firstName + ' ' + lastName + " j'ai " + age + ' ans!');
+let unNouveauMessage = "J'apprend encore l'algorithmie";
+console.log(unNouveauMessage);
 
-let formation = 'Doranco';
-let duration = 2;
-// console.log('Bonjour, je suis une formation à ' + formation + ' depuis ' + duration + ' mois');
+/* --------------------------- Instructions Conditionnelles --------------------------- */
 
-// Les opérations mathématiques ( +, -, *, / )
+let condition = 19;
 
-let result = 2 * 3;
-console.log('je contiens ' + result);
-
-let a = 12;
-let b = 45;
-let resultat = a * b;
-console.log('je contiens ' + resultat);
-
-// Utilisateur donne une valeur
-// let ages =  prompt('Quel est ton age?');
-// console.log(ages);
-
-// On va demander un nombre, on l'affecte dans une variable
-// On va demander un deuxieme nombre, on l'affecte dans une autre variable
-// On affiche "le resultat de la multiplication est : " X
-// let c = prompt('Quel est votre nombre');
-// let d = prompt('Quel est votre nombre');
-// let res = c * d;
-// console.log('le resultat de la multiplication est : ' + res);
-
-/* ------------------ Instructions Conditionnelles ------------------ */
-// let condition = prompt('Quel est ton age ?');
-
-// // Les opérateurs de comparaison (==, !=, >, >=, <, <=)
-// if (condition == 18) {
-//     console.log('bravo, tu as le droit à un cadeau!');
-// } else if (condition < 18) {
-//     console.log('Tu es mineur!');
-// } else {
-//     console.log('Tu es trop vieux!');
-// }
-
-// demander à l'utilisateur s'il a des enfants
-// s'il repond oui
-// demander si c'est un garcon
-// si c'est oui afficher il doit aimer le foot
-// sinon affiche je sais pas ce qu'il aime
-// s'il repond non
-// afficher ce n'est pas un garçon
-// let response = prompt('est-ce que vous avez des enfants?');
-
-// if (response == 'oui') {
-//     let sex = prompt('Est-ce un garçon?');
-//     if (sex == 'oui') {
-//         console.log('il doit aimer le foot')
-//     } else {
-//         console.log("je ne sais ce qu'il aime")
-//     }
-// } else if (response == 'non') {
-//     console.log("ce n'est pas un garçon");
-// }
-
-// Les operateurs logiques OU et ET
-let val = 18;
-
-if (val < 10 || val > 20) {
-} else if (val != 18 && val < 16) {
+// Si (if)
+if (condition == 18) {
+    console.log('tout est ok');
 }
 
-// Ecrire un algorithme qui demande à l'utilisateur son age.
-// On va repondre indique ensuite à l'utilisateur quel film il peut aller voir.
-// "Action Man" si moins de 13 ans
-// "Matrix" si il a entre 13 et 18 ans
-// "Evil Dead" si plus de 18ans
+// Si, Sinon (if, else)
+if (condition == 17) {
+    console.log('tout est ok');
+} else {
+    console.log('Houston, nous avons un problème');
+}
 
-// let userAge = prompt('Quel est ton âge?');
+// Si, Sinon Si, Sinon (if, else if, else)
+if (condition == 17) {
+    console.log('tout est ok');
+} else if (condition == 19) {
+    console.log('là ça va');
+} else if (condition == 20) {
+    console.log('là ça va encore');
+} else {
+    console.log('Houston, nous avons un problème');
+}
 
-// if (userAge < 13) {
-//     console.log('Action Man');
-// } else if (userAge >= 13 && userAge <= 18) {
-//     console.log('Matrix');
-// } else if (userAge < 18) {
-//     console.log('Evil dead');
-// }
+// Les opérateurs de comparaison (==, !=, >, >=, <, <=)
+if (condition >= 18) {
+    console.log('Tu es majeur, bravo');
+} else {
+    console.log('Houston, nous avons un problème');
+}
 
-/* --------------------------- Les boucles -------------------------- */
+// Les opérateurs logique OU (||) et ET (&&)
+let permis = true;
+if (condition >= 18 || permis == true) {
+    console.log('Tu es majeur et tu as le permis');
+} else {
+    console.log('Houston, nous avons 2 problèmes');
+}
+
+// Récupérer une valeur de l'utilisateur
+let age = prompt('quel ages as-tu?');
+
+if (age >= 18) {
+    console.log('tu es majeur!');
+} else {
+    console.log('tu es mineur');
+}
+
+/* --------------------------- Les boucles --------------------------- */
 
 // Boucle à itérations définies (for)
-// for (let i = 0; i <= 10; i++) {
-//     console.log(i);
-// }
-
-// Ecrire une table de multiplication automatiser
-// demander une chiffre à notre utilisateur
-// boucle
-// afficher
-// 9 x 0 = 9
-// 9 x 1 = 9
-// 9 x 2 = 18
-
-// let chiffre = prompt('Quelle table de multiplication voulez-vous?');
-// for (let i = 0; i < 10; i++) {
-//     let result = chiffre * i;
-//     console.log(chiffre + ' x ' + i + ' = ' + result);
-// }
-
-// Écrire un algorithme qui demande un nombre entier non nul de départ,
-// et qui calcule la somme des entiers jusqu’à ce nombre. Par exemple, si l’on entre 5, le programme doit calculer : 1 + 2 + 3 + 4 + 5 = 15.
-
-// let chiffre = prompt('Quelle table de multiplication voulez-vous?');
-
-// let res = 0;
-// for (let i = 0; i <= chiffre; i++) {
-//     res = res + i;
-// }
-
-// console.log(res);
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
 
 // Boucle à itérations non définies (while)
-// let i = 0;
-// let chiffre = prompt('Quel est votre chiffre ?')
-// let sapin ='#';
+let i = 0;
+let diese = '#';
+while (i < 18) {
+    console.log(diese);
+    diese += '#';
+    i++;
+}
+console.log('Enfin fini de tourner en rond!');
 
-// while (i <= chiffre) {
-//     console.log(sapin)
-//     sapin = sapin + '#';
-//     i++;
-// }
-
-// Utilisateur nous donne un nombre
-// tant que on a pas atteint le chiffre
-// #
-// ##
-// ###
+// Boucle Tant Que (while)
+let majeur = prompt('quel ages as-tu?');
+while (majeur < 18) {
+    majeur = prompt('Tu es mineur, tu ne peux pas rentrer!');
+}
 
 /* --------------------------- Les fonctions --------------------------- */
-
-function coucou() {
-    let i = 0;
-    let chiffre = prompt('Quel est votre chiffre ?');
-    let sapin = '#';
-
-    while (i <= chiffre) {
-        console.log(sapin);
-        sapin = sapin + '#';
-        i++;
-    }
-}
-
-// Une fonction n'est executé que lorsqu'elle est appelé
-
-// coucou();
-
-function afficher(x) {
-    console.log(x);
-}
-
-afficher(12);
-
-afficher('test');
 
 function add(x, y) {
     return x + y;
 }
 
-function divide(x, y) {
-    return y / x;
+// Une fonction n'est executé que lorsqu'elle est appelé
+let result = add(7, 3);
+
+// Portée des variables dans une fonction
+// locale ou globale
+function test() {
+    let value = 4;
+    return 2;
 }
 
-let r = divide(4,7)
-console.log(r)
+let res = test();
+console.log(res);
 
-
-let value = add(12, 13);
+// Une variable déclaré dans une fonction n'est pas accessible en dehors de sa fonction
+// Une erreur est généré en tentant de l'afficher
 console.log(value);
 
-// créer un fonction qui aura un parametre
-// return le carre de ce parametre
-// appelle la fonction on la recupère la valeur dans une variable et on l'affiche
+/* --------------------------- Les tableaux --------------------------- */
+
+let vegetables = ['pain', 'lait', 'fromage', 'houmous', 'nouilles'];
+
+// On peut afficher une case d'un tableau via son indice
+// ! Attention l'indice d'un tableau commence à 0
+console.log(vegetables[4]);
+
